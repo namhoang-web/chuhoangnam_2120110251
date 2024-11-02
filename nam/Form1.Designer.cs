329fdee3a23e820a8cbaaf38185a55f893bdd47c
namhoang-web <hoangnammony@gmail.com>
2024-11-02 13:42:59 +0700

    nam

---
+++ nam/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace nam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Biến điều khiển cần thiết.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Dọn dẹp tài nguyên đang sử dụng.
+        /// </summary>
+        /// <param name="disposing">true nếu tài nguyên không quản lý cần được hủy; ngược lại, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Mã tạo
+
+        /// <summary>
+        /// Phương thức yêu cầu thiết lập các thành phần.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeaderId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderAddress = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderPhone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(100, 20);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(200, 20);
+            this.txtId.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(100, 60);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(200, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(100, 100);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(200, 20);
+            this.txtAddress.TabIndex = 2;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(100, 140);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(200, 20);
+            this.txtPhone.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(320, 23);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(92, 39);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(458, 20);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(92, 42);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Sửa";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(320, 121);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(92, 39);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderId,
+            this.columnHeaderName,
+            this.columnHeaderAddress,
+            this.columnHeaderPhone});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(20, 180);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(554, 200);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeaderId
+            // 
+            this.columnHeaderId.Text = "Id";
+            this.columnHeaderId.Width = 50;
+            // 
+            // columnHeaderName
+            // 
+            this.columnHeaderName.Text = "Tên";
+            this.columnHeaderName.Width = 100;
+            // 
+            // columnHeaderAddress
+            // 
+            this.columnHeaderAddress.Text = "Địa chỉ";
+            this.columnHeaderAddress.Width = 150;
+            // 
+            // columnHeaderPhone
+            // 
+            this.columnHeaderPhone.Text = "Số điện thoại";
+            this.columnHeaderPhone.Width = 100;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Tên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Địa Chỉ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "SĐT";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(458, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 39);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Thoát";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(599, 401);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtId);
+            this.Name = "Form1";
+            this.Text = "Quản lý sinh viên";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeaderId;
+        private System.Windows.Forms.ColumnHeader columnHeaderName;
+        private System.Windows.Forms.ColumnHeader columnHeaderAddress;
+        private System.Windows.Forms.ColumnHeader columnHeaderPhone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ nam/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
+
+namespace nam
+{
+    public partial class Form1 : Form
+
+    {
+        private SqlConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+            string connectionString = "Server=.;Database=Student;Integrated Security=True;";
+            connection = new SqlConnection(connectionString);
+            LoadStudents();
+        }
+        private void LoadStudents()
+        {
+            connection.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Students", connection);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            connection.Close();
+
+            listView1.Items.Clear();
+            foreach (DataRow row in dt.Rows)
+            {
+                ListViewItem item = new ListViewItem(row["Id"].ToString());
+                item.SubItems.Add(row["Name"].ToString());
+                item.SubItems.Add(row["Address"].ToString());
+                item.SubItems.Add(row["Phone"].ToString());
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string address = txtAddress.Text;
+            string phone = txtPhone.Text;
+
+            connection.Open();
+            SqlCommand command = new SqlCommand("INSERT INTO Students (Name, Address, Phone) VALUES (@name, @address, @phone)", connection);
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@address", address);
+            command.Parameters.AddWithValue("@phone", phone);
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            LoadStudents();
+            ClearFields();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn sinh viên để sửa.");
+                return;
+            }
+
+            int id = Convert.ToInt32(listView1.SelectedItems[0].Text);
+            string name = txtName.Text;
+            string address = txtAddress.Text;
+            string phone = txtPhone.Text;
+
+            connection.Open();
+            SqlCommand command = new SqlCommand("UPDATE Students SET Name=@name, Address=@address, Phone=@phone WHERE Id=@id", connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@address", address);
+            command.Parameters.AddWithValue("@phone", phone);
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            LoadStudents();
+            ClearFields();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn sinh viên để xóa.");
+                return;
+            }
+
+            int id = Convert.ToInt32(listView1.SelectedItems[0].Text);
+
+            connection.Open();
+            SqlCommand command = new SqlCommand("DELETE FROM Students WHERE Id=@id", connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            LoadStudents();
+            ClearFields();
+        }
+
+        private void ClearFields()
+        {
+            txtId.Text = "";
+            txtName.Text = "";
+            txtAddress.Text = "";
+            txtPhone.Text = "";
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+                return;
+
+            ListViewItem item = listView1.SelectedItems[0];
+            txtId.Text = item.Text;
+            txtName.Text = item.SubItems[1].Text;
+            txtAddress.Text = item.SubItems[2].Text;
+            txtPhone.Text = item.SubItems[3].Text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+   
